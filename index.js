4be897610aaadbe7282b07632e9bdec14ba0e104
Kunal malviya <kunal.malviya351@gmail.com>
2021-01-30 20:35:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+* Call a function before page load
+**/
+<script type="text/javascript">
+    function preloadFunc(){
+  		//userRedirection()
+    }
+    window.onpaint = preloadFunc();
+</script>
