57389aa58b06358c5e63cf7cc87a30beadaaa89f
Kunal malviya <kunal.malviya351@gmail.com>
2021-05-10 18:05:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,18 @@
     }
     window.onpaint = preloadFunc();
 </script>
+
+/**
+* This function will find localstorage value by key matching
+* "#user"
+**/
+function getLocalStorageByKeyMatch(key) {
+    var item = null
+    for (var i = 0; i < localStorage.length; i++) {
+        if ( localStorage.key(i).indexOf(key) != -1 ) {
+            item = localStorage.getItem(localStorage.key(i));
+            break;
+        }
+    }
+    return item
+}
