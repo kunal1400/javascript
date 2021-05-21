6457dd948a606111e13411c08e54e753d6e08712
Kunal malviya <kunal.malviya351@gmail.com>
2021-05-21 17:26:21 +0530

    Update Form.js

--- Form.js
+++ Form.js
@@ -77,3 +77,32 @@ function convertSerializeArrayToObject( serializeArray ) {
     });
     return o;
 }
+
+/**
+* Javascript file upload and handling
+**/
+function handleFileSelect() {
+	if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
+	  alert('The File APIs are not fully supported in this browser.');
+	  return;
+	}   
+
+	var input = document.getElementById('fileinput');
+	if (!input) {
+	  alert("Um, couldn't find the fileinput element.");
+	}
+	else if (!input.files) {
+	  alert("This browser doesn't seem to support the `files` property of file inputs.");
+	}
+	else if (!input.files[0]) {
+	  alert("Please select a file before clicking 'Load'");               
+	}
+	else {
+	  var file = input.files[0];
+	  var fr = new FileReader();
+	  fr.onload = receivedText;
+	  //fr.readAsText(file);
+	  //fr.readAsBinaryString(file); //as bit work with base64 for example upload to server
+	  fr.readAsDataURL(file);
+	}
+}      
