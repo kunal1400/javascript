455fa674fe5f445934ad2d2cdaa93a39a8bf6237
Kunal malviya <kunal.malviya351@gmail.com>
2021-06-20 11:38:15 +0530

    Update fileUpload.js

--- stringFunctions.js/fileUpload.js
+++ stringFunctions.js/fileUpload.js
@@ -2,4 +2,25 @@
 * Replace all illegal character and spaces from file name except .
 * https://stackoverflow.com/questions/13020246/remove-special-symbols-and-extra-spaces-and-replace-with-underscore-using-the-re
 **/
-let uploadFileName = str.replace(/[^A-Z0-9.]+/ig, "_");
+const fixFileName = ( str ) => {
+	return str.replace(/[^A-Z0-9.]+/ig, "_");
+}
+
+/**
+* Get base64 mimetype
+**/
+const base64MimeType = (encoded) => {
+  var result = null;
+
+  if (typeof encoded !== 'string') {
+    return result;
+  }
+
+  var mime = encoded.match(/data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/);
+
+  if (mime && mime.length) {
+    result = mime[1];
+  }
+
+  return result;
+}
